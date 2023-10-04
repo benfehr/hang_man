6718936b3ec8c0ecb803cff96b997f8168ca7428
benfehr <benjamin_louis_foote@hotmail.com>
2023-10-04 12:57:38 +0000

    Define a function called play that sets the rules for gameplay

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+
